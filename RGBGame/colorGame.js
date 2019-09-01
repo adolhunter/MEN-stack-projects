39b929c9d57a10a19f1d96ee5c8d0d30df3c00ff
adolhunter <mingshiwang123@gmail.com>
2019-09-01 16:08:38 -0500

    buttonAdded

--- RGBGame/colorGame.js
+++ RGBGame/colorGame.js
@@ -11,7 +11,32 @@ var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay=document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click", function() {
+    easyBtn.classList.add("selected");
+    hardBtn.classList.remove("selected");
+});
+hardBtn.addEventListener("click", function() {
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");
+});
+
+
 colorDisplay.textContent = pickedColor; 
+
+resetButton.addEventListener("click", function() {
+    colors = generateRandomColors(6);
+    pickedColor=pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+    }
+    h1.style.backgroundColor = "#232323";
+});
 
 for (var i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
@@ -19,7 +44,9 @@ for (var i = 0; i < squares.length; i++) {
        var clickedColor = this.style.backgroundColor;
        if (clickedColor === pickedColor) {
            messageDisplay.textContent = "Correct!"; 
+           resetButton.textContent = "Play Again?";
            changeColors(clickedColor);
+           h1.style.backgroundColor = clickedColor;
        } else {
            this.style.backgroundColor="#232323";
            messageDisplay.textContent = "Try again!";
