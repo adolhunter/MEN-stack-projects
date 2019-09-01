9b156a156afde7329c71abd98fa4fc9d211e90af
adolhunter <mingshiwang123@gmail.com>
2019-09-01 15:24:21 -0500

    colorChange

--- RGBGame/colorGame.js
+++ RGBGame/colorGame.js
@@ -9,7 +9,7 @@ var colors = ["rgb(255, 0, 0)",
 var squares = document.querySelectorAll(".square");
 var pickedColor = colors[3];
 var colorDisplay=document.getElementById("colorDisplay");
-
+var messageDisplay = document.querySelector("#message");
 colorDisplay.textContent = pickedColor; 
 
 for (var i = 0; i < squares.length; i++) {
@@ -17,10 +17,17 @@ for (var i = 0; i < squares.length; i++) {
     squares[i].addEventListener("click", function() {
        var clickedColor = this.style.backgroundColor;
        if (clickedColor === pickedColor) {
-           alert("correct");
+           messageDisplay.textContent = "Correct!"; 
+           changeColors(clickedColor);
        } else {
-           alert("wrong!");
+           this.style.backgroundColor="#232323";
+           messageDisplay.textContent = "Try again!";
        }
     });
 }
 
+function changeColors(color) {
+    for (var i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = color;
+    }
+}
