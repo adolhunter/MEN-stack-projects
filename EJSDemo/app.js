04342ebc5b319c3513266342fcdd948a6adb860a
adolhunter <mingshiwang123@gmail.com>
2019-09-05 16:18:36 -0500

    partials

--- EJSDemo/app.js
+++ EJSDemo/app.js
@@ -1,13 +1,27 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
 app.get("/", (req, res) => {
-    res.render("home.ejs");
+    res.render("home");
 });
 
 app.get("/fallinlovewith/:thing", (req, res) => {
-    res.render("love.ejs", { thingVar: req.params.thing });
+    res.render("love", { thingVar: req.params.thing });
 });
+
+app.get("/posts", (req, res) => {
+    var posts = [
+        { title: "post 1", author: "Susy" },
+        { title: "My adorable pet bunny", author: "Charlie" },
+        { title: "Can you believe this pomsky?", author: "Colt" }
+    ];
+
+    res.render("posts", { posts: posts })
+});
+
 
 
 app.listen(3000, function () {
