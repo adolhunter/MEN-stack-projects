0478a52586b0ddbb425e17b87db182bc2c4b7a72
adolhunter <mingshiwang123@gmail.com>
2019-09-06 16:06:22 -0500

    search functionality

--- MovieSearchApp/app.js
+++ MovieSearchApp/app.js
@@ -3,10 +3,18 @@ var app = express();
 var request = require("request");
 app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
+    res.render("search");
+});
+
+
+
 app.get("/results", (req, res) => {
-    request("http://www.omdbapi.com/?s=san diego&apikey=thewdb", (error, response, body) => {
+    var query = req.query.search;
+    var url = "http://omdbapi.com/?apikey=thewdb&s=" + query;
+    request(url, (error, response, body) => {
         if (!error && response.statusCode == 200) {
-            var data = JSON.parse(body);
+            var data = JSON.parse(body)
             res.render("results", { data: data });
         }
     });
