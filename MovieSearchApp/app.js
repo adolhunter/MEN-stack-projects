ecca8d20e0c4bbf741bae1a668bbe836782ae5c8
adolhunter <mingshiwang123@gmail.com>
2019-09-06 15:36:01 -0500

    basic function

---
+++ MovieSearchApp/app.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+
+app.get("/results", (req, res) => {
+    request("http://www.omdbapi.com/?s=california&apikey=thewdb", (error, response, body) => {
+        if (!error && response.statusCode == 200) {
+            var results = JSON.parse(body);
+            res.send(results.Search[0].Title);
+        }
+    });
+});
+
+
+app.listen(3000, () => {
+    console.log("Movie server started!");
+});
