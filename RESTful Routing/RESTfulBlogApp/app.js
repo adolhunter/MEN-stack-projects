8e26018d0b7ba7f6a882fc015b19a4e122333a56
adolhunter <mingshiwang123@gmail.com>
2019-09-09 16:11:08 -0500

    index route

--- RESTful Routing/RESTfulBlogApp/app.js
+++ RESTful Routing/RESTfulBlogApp/app.js
@@ -8,14 +8,37 @@ mongoose.connect('mongodb://localhost:27017/restful_blog_app', {
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
-app.set("view enginer", "ejs");
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-//title
-//image
-//body
-//created
 
+//Mongoose setup
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: { type: Date, default: Date.now }
+})
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+
+//RESTful routes
+app.get("/", (req, res) => {
+    res.redirect("/blogs");
+});
+
+
+
+app.get("/blogs", (req, res) => {
+    Blog.find({}, (err, blogs) => {
+        if (err) {
+            console.log("error");
+        } else {
+            res.render("index", {blogs:blogs});
+        }
+    });
+})
 
 
 app.listen(3000, () => {
