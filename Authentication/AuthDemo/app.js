e9b7439949f982f43ba50b63f486f759b7a9090b
adolhunter <mingshiwang123@gmail.com>
2019-09-21 20:33:04 -0500

    setup for auth

--- Authentication/AuthDemo/app.js
+++ Authentication/AuthDemo/app.js
@@ -1,15 +1,36 @@
-var express = require("express");
-var mongoose = require("mongoose");
-mongoose.connect('mongodb://localhost:27017/auth_demo_app', { useNewUrlParser: true });
+var express = require("express"),
+    mongoose = require("mongoose"),
+    passport = require("passport"),
+    bodyParser = require("body-parser"),
+    User = require("./models/user"),
+    LocalStrategy = require("passport-local"),
+    passportLocalMongoose = require("passport-local-mongoose");
+
+mongoose.connect('mongodb://localhost:27017/auth_demo_app', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 var app = express();
-app.set("view engine", "ejs");
 
-app.get("/", (req,res) => {
+app.use(require("express-session")({
+    secret: "Hey what are you looking at",
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.set("view engine", "ejs");
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", (req, res) => {
     res.render("home");
 });
 
-app.get("/secret", (req,res) => {
+app.get("/secret", (req, res) => {
     res.render("secret");
 });
 
