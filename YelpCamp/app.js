4f90f192554fed103b4933a239ee0cc8fd9ccdd1
adolhunter <mingshiwang123@gmail.com>
2019-09-21 19:58:58 -0500

    styling changes

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -10,7 +10,7 @@ seedDB();
 mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
-
+app.use(express.static(__dirname + "/public"))
 
 
 
@@ -97,9 +97,9 @@ app.post("/campgrounds/:id/comments", function (req, res) {
             });
         }
     });
-        //create new comment
-        //connect new comment to campground
-        //redirect to campground showpage
+    //create new comment
+    //connect new comment to campground
+    //redirect to campground showpage
 
 });
 
