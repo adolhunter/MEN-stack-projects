588ef26bd0b240bf6875d981ebac74da741166b5
adolhunter <mingshiwang123@gmail.com>
2019-09-29 14:36:18 -0500

    finished all authentication

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -26,6 +26,11 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+app.use((req,res,next) => {
+    res.locals.currentUser = req.user;
+    next();
+});
+
 
 app.get("/", (req, res) => {
     res.render("landing");
@@ -37,7 +42,7 @@ app.get("/campgrounds", (req, res) => {
         if (err) {
             console.log(err);
         } else {
-            res.render("campgrounds/index", { campgrounds: allCampgrounds })
+            res.render("campgrounds/index", { campgrounds: allCampgrounds})
         }
     });
 });
