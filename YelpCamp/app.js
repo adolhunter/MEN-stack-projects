1f9dea5ef9f2ae56ed3dfb80c8297c96b15b872b
adolhunter <mingshiwang123@gmail.com>
2019-09-30 16:43:21 -0500

    added update and edit route for campgrounds

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -5,9 +5,10 @@ mongoose = require("mongoose");
 passport = require("passport");
 LocalStrategy = require("passport-local");
 Campground = require("./models/campground");
-Comment = require("./models/comment")
-User = require("./models/user")
+Comment = require("./models/comment");
+User = require("./models/user");
 seedDB = require("./seeds");
+methodOverride = require("method-override");
 
 //requiring routes
 var commentRoutes = require("./routes/comments"),
@@ -19,6 +20,7 @@ mongoose.connect('mongodb://localhost:27017/yelp_camp', { useNewUrlParser: true 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"))
+app.use(methodOverride("_method"));
 
 //passport configuration
 app.use(require("express-session")({
