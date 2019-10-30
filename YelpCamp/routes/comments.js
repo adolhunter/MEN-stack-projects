bb8c14bd89e68022fd9a600be45d917c0853a92a
adolhunter <mingshiwang123@gmail.com>
2019-10-30 13:43:45 -0500

    edit and update

--- YelpCamp/routes/comments.js
+++ YelpCamp/routes/comments.js
@@ -41,6 +41,31 @@ router.post("/", isLoggedIn, function (req, res) {
     });
 });
 
+
+//Edit route
+router.get("/:comment_id/edit", (req, res) => {
+    Comment.findById(req.params.comment_id, (err, foundComment) => {
+        if (err) {
+            res.redirect("back");
+        } else {
+            // console.log(foundComment);
+            res.render("comments/edit", { campground_id: req.params.id, comment: foundComment });
+        }
+    })
+});
+
+//Comment update
+router.put("/:comment_id", (req, res) => {
+    Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" + req.params.id);
+        }
+    })
+})
+
+
 //middleware
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
