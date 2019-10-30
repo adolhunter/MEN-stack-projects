9d077bf8ab7b2adfa287eff315807a7e165b9a37
adolhunter <mingshiwang123@gmail.com>
2019-10-30 13:59:36 -0500

    comment destroy

--- YelpCamp/routes/comments.js
+++ YelpCamp/routes/comments.js
@@ -66,6 +66,20 @@ router.put("/:comment_id", (req, res) => {
 })
 
 
+//Comment Destroy Route
+router.delete("/:comment_id", (req,res) => {
+    Comment.findByIdAndRemove(req.params.comment_id, (err) => {
+        if (err) {
+            res.redirect("back");
+        } else {
+            res.redirect("/campgrounds/" +req.params.id);
+        }
+    })
+})
+
+
+
+
 //middleware
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) {
