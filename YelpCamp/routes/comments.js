c772097ab2fa1b57690872bfad3d8211ee41fb3f
adolhunter <mingshiwang123@gmail.com>
2019-10-30 14:30:16 -0500

    comment authorization

--- YelpCamp/routes/comments.js
+++ YelpCamp/routes/comments.js
@@ -43,7 +43,7 @@ router.post("/", isLoggedIn, function (req, res) {
 
 
 //Edit route
-router.get("/:comment_id/edit", (req, res) => {
+router.get("/:comment_id/edit", checkCommentOwnership, (req, res) => {
     Comment.findById(req.params.comment_id, (err, foundComment) => {
         if (err) {
             res.redirect("back");
@@ -55,7 +55,7 @@ router.get("/:comment_id/edit", (req, res) => {
 });
 
 //Comment update
-router.put("/:comment_id", (req, res) => {
+router.put("/:comment_id", checkCommentOwnership, (req, res) => {
     Comment.findByIdAndUpdate(req.params.comment_id, req.body.comment, (err, updatedComment) => {
         if (err) {
             res.redirect("back");
@@ -67,12 +67,12 @@ router.put("/:comment_id", (req, res) => {
 
 
 //Comment Destroy Route
-router.delete("/:comment_id", (req,res) => {
+router.delete("/:comment_id", checkCommentOwnership, (req, res) => {
     Comment.findByIdAndRemove(req.params.comment_id, (err) => {
         if (err) {
             res.redirect("back");
         } else {
-            res.redirect("/campgrounds/" +req.params.id);
+            res.redirect("/campgrounds/" + req.params.id);
         }
     })
 })
@@ -86,6 +86,25 @@ function isLoggedIn(req, res, next) {
         return next();
     }
     res.redirect("/login");
+}
+
+function checkCommentOwnership(req, res, next) {
+    if (req.isAuthenticated()) {
+        Comment.findById(req.params.comment_id, (err, foundComment) => {
+            if (err) {
+                res.redirect("back");
+            } else {
+                //does user own the campground, if not redirect
+                if (foundComment.author.id.equals(req.user._id)) {
+                    next();
+                } else {
+                    res.redirect("back");
+                }
+            }
+        });
+    } else {
+        res.redirect("back");
+    }
 }
 
 
