b8b3e90ff2a16bcacc157c4cac014c724fd1bbb2
adolhunter <mingshiwang123@gmail.com>
2019-11-04 12:09:56 -0600

    dynamic price

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -19,12 +19,13 @@ router.post("/", middleware.isLoggedIn, (req, res) => {
     // get data from form and add to campground array
     var name = req.body.name;
     var image = req.body.image;
+    var price = req.body.price;
     var description = req.body.description;
     var author = {
         id: req.user._id,
         username: req.user.username
     }
-    var newCampground = { name: name, image: image, description: description, author: author };
+    var newCampground = { name: name, image: image, description: description, author: author, price: price };
     //create a new campground and save to DB
     Campground.create(newCampground, (err, newlyCreated) => {
         if (err) {
